6e13e8a2414e7e7e717f9da0f7fede4c8ebbd9dc
Adiomojeed <adio.mojeed@gmail.com>
2020-04-03 23:13:43 +0100

    Implemented screen III design

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router,  Route } from "react-router-dom";
 import "./styles/app.scss";
 import SearchPage from "./components/SearchPage";
+import Dashboard from './components/Dashboard'
 
 const App = () => {
     return (
-        <React.Fragment>
-            <SearchPage />
-        </React.Fragment>
+        <Router>
+            <React.Fragment>
+                <Route exact path='/' component={SearchPage} />
+                <Route path='/dashboard' component={Dashboard} />
+            </React.Fragment>
+        </Router>
     );
 };
 
